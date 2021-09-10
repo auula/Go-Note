e056e289939d4986475af3c72b1fb249967f0bd7
zoulingbin <466483158@qq.com>
2021-09-10 13:46:10 +0800

    interface

--- code/interface.go
+++ code/interface.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+
+//断言
+
 type Animals interface {
 	Run() string
 	Call() string
@@ -19,9 +22,25 @@ func (c *Cats) Run() string{
 	return "Run"
 }
 
+type Dogs struct {
+
+}
+
+func (d *Dogs) Call() string  {
+	return "dogs call"
+}
+
+func (d *Dogs) Run() string {
+	return "dog run"
+}
+
 func main() {
-	c := &Cats{"cat"}
-	var a Animals
-	a = c
-	fmt.Println(a.Call())
+	z := &Cats{"cat"}
+	var b Animals
+	b = z
+	if v, ok := b.(*Dogs); ok {
+		fmt.Println(v)
+	}else{
+		fmt.Println("error")
+	}
 }
